6734de1fbe65f55fd67b6dc6ccbda7c085fb458a
Sergio <sergioruza22@gmail.com>
2023-03-08 12:05:41 -0300

    afafa

--- desafio9.sql
+++ desafio9.sql
@@ -1,3 +1,7 @@
-SELECT count(cancoes_id) FROM SpotifyClone.historico AS hist
-INNER JOIN SpotifyClone.usuario as us ON hist.usuario_id = us.usuario_id
-WHERE us.nome = 'Barbara Liskov';
+SELECT
+  count(cancoes_id)
+FROM
+  SpotifyClone.historico AS hist
+  INNER JOIN SpotifyClone.usuario as us ON hist.usuario_id = us.usuario_id
+WHERE
+  us.nome = 'Barbara Liskov';
